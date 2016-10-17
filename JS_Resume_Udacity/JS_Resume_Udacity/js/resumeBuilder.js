2c288b2b8339eb69ed2b6e9ad6d5ebc626860297
Heinz <Heinz@HEINZ-PC>
2016-10-16 18:59:52 -0700

    Still learning the super basics

---
+++ JS_Resume_Udacity/JS_Resume_Udacity/js/resumeBuilder.js
@@ -0,0 +1,16 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+
+//var name = "Timothy Krause";
+//var role = "Web Dev";
+
+//var formattedName = HTMLheaderName.replace("%data%", name);
+//var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+
+//var skills = ["OOP Programming", "Customer Service", "Python"];
+
+//$("#main").append(skills.length);
